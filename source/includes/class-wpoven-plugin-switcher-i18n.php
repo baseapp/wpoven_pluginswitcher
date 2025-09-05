0e8fd0f596f0bc64edb546ab1a92c6fab0517dea
shyambaseapp <shyam.baseapp@gmail.com>
2025-09-05 16:47:21 +0530

    ignore load_plugin_textdomain

--- source/includes/class-wpoven-plugin-switcher-i18n.php
+++ source/includes/class-wpoven-plugin-switcher-i18n.php
@@ -31,23 +31,16 @@ class Wpoven_Plugin_Switcher_i18n
 	/**
 	 * Load the plugin text domain for translation.
 	 *
-	 * @since 1.0.0
+	 * @since    1.0.0
 	 */
 	public function load_plugin_textdomain()
 	{
 
-		// Since WP 4.6, WordPress.org plugins don’t need load_plugin_textdomain().
-		// This fallback ensures translations still load if distributed outside wp.org.
-		$domain = 'wpoven-plugin-switcher';
+		// load_plugin_textdomain(
+		// 	'wpoven-plugin-switcher',
+		// 	false,
+		// 	dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		// );
 
-		// First, try to load from WordPress.org language packs (auto-handled).
-		if (! is_textdomain_loaded($domain)) {
-			// Fallback: load from /languages directory inside the plugin.
-			load_plugin_textdomain(
-				$domain,
-				false,
-				dirname(dirname(plugin_basename(__FILE__))) . '/languages/'
-			);
-		}
 	}
 }
